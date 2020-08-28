b20339bf0320272961ed6eae6b41863bca00427a
sbuergers <sbuergers@gmail.com>
2020-08-27 23:06:37 -0400

    changed recommend.py to work with postgres SQL DB instead of pandas DF from .csv file

--- recommend.py
+++ recommend.py
@@ -1,63 +1,69 @@
+from psycopg2 import sql
 import pandas as pd
-import numpy as np
-# import datetime as dt
 
 
-# Load recipe data review data
-recipes = pd.read_csv(r'./data/recipes_sql.csv', index_col=0,
-                      dtype={
-                        'title': str,
-                        'ingredients': str,
-                        'directions': str,
-                        'categories': str,
-                        'date': str,              # should probably be datetime
-                        'desc': str,
-                        'rating': np.float64,
-                        'calories': np.float64,
-                        'sodium': np.float64,
-                        'fat': np.float64,
-                        'protein': np.float64,
-                        'ghg': np.float64,
-                        'prop_ing': np.float64,
-                        'ghg_log10': np.float64,
-                        'url': str,
-                        'servings': str,
-                        'index': np.int64,
-                        'image_url': str,
-                        'rating_count': np.int64
-                      })
-
-# Load review data
-reviews = pd.read_csv(r'./data/reviews_sql.csv', index_col=0)
-
-# Load recommendation model information
-CS_ids = pd.read_csv(r'./data/content_similarity_200_ids.csv', index_col=0)
-CS = pd.read_csv(r'./data/content_similarity_200.csv', index_col=0)
-
-
-def content_based_search(search_term):
+def content_based_search(search_term, cur):
     '''
     DESCRIPTION:
         return the 200 most similar recipes to the url defined
         in <search term> based on cosine similarity in the "categories"
-        space of the epicurious dataset (i.e. recipes data frame).
+        space of the epicurious dataset.
     INPUT:
     search_term (str)
         url identifier for recipe (in recipes['url'])
+    cur (psycopg2 connection)
     OUTPUT:
     results (dataframe)
         Recipe dataframe similar to recipes, but
         containing only the Nsim most similar recipes to the
         input. Also contains additional column "similarity".
     '''
-    # obtain IDs of similar recipes
-    recipe_id = recipes.index[recipes['url'] == search_term].values
-    similar_recipe_ids = abs(CS_ids.loc[recipe_id, :]).values
+    # Select recipe IDs of 200 most similar recipes to reference (search_term)
+    cur.execute(sql.SQL("""
+                SELECT * FROM public.content_similarity200_ids AS csids
+                WHERE "recipeID" = (
+                    SELECT "recipesID" FROM public.recipes
+                    WHERE url = %s)
+                """).format(), [search_term])
+    CS_ids = cur.fetchall()[0][1::]
+    CS_ids = tuple([abs(CSid) for CSid in CS_ids])
 
-    # prepare output dataframe
-    results = recipes.loc[similar_recipe_ids[0], :]
-    results['similarity'] = CS.loc[recipe_id, :].values[0]
+    # Also select the actual similarity scores
+    cur.execute(sql.SQL("""
+                SELECT * FROM public.content_similarity200
+                WHERE "recipeID" = (
+                    SELECT "recipesID" FROM public.recipes
+                    WHERE url = %s)
+                """).format(), [search_term])
+    CS = cur.fetchall()[0][1::]
+    CS = tuple([float(abs(s)) for s in CS])
+
+    # Finally, select similar recipes themselves
+    # Get only those columns I actually use to speed things up
+    col_sel = [
+            'recipesID', 'title', 'ingredients', 'rating', 'calories', 'sodium', 
+            'fat', 'protein', 'ghg', 'prop_ing', 'ghg_log10', 'url', 'servings', 
+            'index'
+            ]
+
+    # I am aware there is a discrepancy between the column names in SQL
+    # and python, which is rather ugly. It is on my todo list to fix
+    # in the future.
+    cur.execute(sql.SQL("""
+                SELECT "recipesID", "title", "ingredients",
+                    "rating", "calories", "sodium", "fat",
+                    "protein", "emissions", "prop_ingredients", 
+                    "emissions_log10", "url", "servings", "recipe_rawid"
+                FROM public.recipes
+                WHERE "recipesID" IN %s
+                """).format(), [CS_ids])
+    recipes_sql = cur.fetchall()
+
+    # Obtain a dataframe for further processing
+    results = pd.DataFrame(recipes_sql, columns=col_sel)
+    results['similarity'] = CS
 
     return results
+
 
 # eof
