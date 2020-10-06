aa3b373e5e0deac2b2fff0b17a960707063f5522
sbuergers <sbuergers@gmail.com>
2020-10-06 14:45:56 -0400

    removed .pem files

--- app.py
+++ app.py
@@ -69,47 +69,47 @@ def create_app(testing=False, debug=True):
 
     # Content security policy (CSP)
     SELF = "'self'"
+    csp = {
+        'default-src': [
+            SELF
+        ],
+        'style-src': [
+            SELF,
+            'https://use.fontawesome.com',
+            'https://stackpath.bootstrapcdn.com',
+            'https://cdn.jsdelivr.net'
+        ],
+        'style-src-elem': [
+            SELF,
+            'https://use.fontawesome.com',
+            'https://stackpath.bootstrapcdn.com',
+            'https://cdn.jsdelivr.net'
+        ],
+        'script-src': [
+            SELF,
+            'https://code.jquery.com',
+            'https://cdnjs.cloudflare.com',
+            'https://stackpath.bootstrapcdn.com',
+            'https://cdn.jsdelivr.net'
+        ],
+        'font-src': [
+            SELF,
+            'https://use.fontawesome.com',
+            'https://stackpath.bootstrapcdn.com'
+        ],
+        'connect-src': [
+            SELF,
+            'https://cdn.jsdelivr.net'
+        ],
+        'frame-src': [
+            SELF
+        ],
+        'frame-ancestors': [
+            SELF
+        ],
+        'img-src': '*'
+    }
     if not testing and not debug:
-        csp = {
-            'default-src': [
-                SELF
-            ],
-            'style-src': [
-                SELF,
-                'https://use.fontawesome.com',
-                'https://stackpath.bootstrapcdn.com',
-                'https://cdn.jsdelivr.net'
-            ],
-            'style-src-elem': [
-                SELF,
-                'https://use.fontawesome.com',
-                'https://stackpath.bootstrapcdn.com',
-                'https://cdn.jsdelivr.net'
-            ],
-            'script-src': [
-                SELF,
-                'https://code.jquery.com',
-                'https://cdnjs.cloudflare.com',
-                'https://stackpath.bootstrapcdn.com',
-                'https://cdn.jsdelivr.net'
-            ],
-            'font-src': [
-                SELF,
-                'https://use.fontawesome.com',
-                'https://stackpath.bootstrapcdn.com'
-            ],
-            'connect-src': [
-                SELF,
-                'https://cdn.jsdelivr.net'
-            ],
-            'frame-src': [
-                SELF
-            ],
-            'frame-ancestors': [
-                SELF
-            ],
-            'img-src': '*'
-        }
         # secure (prod)
         Talisman(
             app,
