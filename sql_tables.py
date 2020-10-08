f24e155bd583284d0d0f75546eb83f510db1bb9d
sbuergers <sbuergers@gmail.com>
2020-10-08 11:10:23 -0400

    added __repr__ method to User class in sql_tables.py - more readable output when calling objects

--- sql_tables.py
+++ sql_tables.py
@@ -11,3 +11,9 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
     password = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return '<User {0}>'.format(self.username)
+
+
+# eof
