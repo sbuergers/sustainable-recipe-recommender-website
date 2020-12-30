2965ac2e2bc4b38b43d327cb6eea91fbaace079b
sbuergers <sbuergers@gmail.com>
2020-12-30 11:26:03 -0500

    added route for terms and conditions page

--- application/auth/routes.py
+++ application/auth/routes.py
@@ -75,4 +75,9 @@ def logout():
     return redirect(url_for('main.home'))
 
 
+@bp.route("/terms_and_conditions")
+def terms_and_conditions():
+    return render_template('terms_and_conditions.html')
+
+
 # eof
