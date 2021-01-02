662f84064debfba7729c9a1af5a61d5c5501267f
sbuergers <sbuergers@gmail.com>
2021-01-02 12:34:49 -0500

    added route reset_password

--- application/auth/routes.py
+++ application/auth/routes.py
@@ -10,7 +10,7 @@ from passlib.hash import pbkdf2_sha512
 
 # User made modules
 from application.auth.forms import RegistrationForm, LoginForm, \
-    ResetPasswordRequestForm
+    ResetPasswordRequestForm, ResetPasswordForm
 
 # Database
 from application import db
@@ -99,6 +99,22 @@ def reset_password_request():
         return redirect(url_for('login'))
     return render_template('reset_password_request.html',
                            title='Reset Password', form=form)
+
+
+@bp.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for('index'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash('Your password has been reset.')
+        return redirect(url_for('login'))
+    return render_template('reset_password.html', form=form)
 
 
 # eof
